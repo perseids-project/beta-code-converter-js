8358884655c840fe2edd5c745528bc46c1438446
zfletch <zfletch2@gmail.com>
2019-02-26 12:00:21 -0500

    Updates

---
+++ precache-manifest.c08395903e5e3500618fca683e1ee7f4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1be2f67f66b74bf4440",
+    "url": "/beta-code-js/static/css/main.ac85b988.chunk.css"
+  },
+  {
+    "revision": "b1be2f67f66b74bf4440",
+    "url": "/beta-code-js/static/js/main.b1be2f67.chunk.js"
+  },
+  {
+    "revision": "52b31a412fb4c193a006",
+    "url": "/beta-code-js/static/js/runtime~main.52b31a41.js"
+  },
+  {
+    "revision": "d631146912c7dbf190d6",
+    "url": "/beta-code-js/static/css/2.04c9a3b7.chunk.css"
+  },
+  {
+    "revision": "d631146912c7dbf190d6",
+    "url": "/beta-code-js/static/js/2.d6311469.chunk.js"
+  },
+  {
+    "revision": "aed4bf22386886ec834caa5f172094c2",
+    "url": "/beta-code-js/static/media/tinos-latin-700.aed4bf22.woff2"
+  },
+  {
+    "revision": "fac0d226a0248b5bf601bdf963a06774",
+    "url": "/beta-code-js/static/media/tinos-latin-400italic.fac0d226.woff"
+  },
+  {
+    "revision": "7b30eff6751f724235b4b243680584df",
+    "url": "/beta-code-js/static/media/tinos-latin-400italic.7b30eff6.woff2"
+  },
+  {
+    "revision": "44ed60cdbe6db7de024a4cddf8ea6f50",
+    "url": "/beta-code-js/static/media/tinos-latin-400.44ed60cd.woff2"
+  },
+  {
+    "revision": "c4c6014641239a3fd358f5e7bbe5bbf6",
+    "url": "/beta-code-js/static/media/tinos-latin-400.c4c60146.woff"
+  },
+  {
+    "revision": "9dd3cad0a4944665a3e4178957a133d0",
+    "url": "/beta-code-js/static/media/tinos-latin-700italic.9dd3cad0.woff"
+  },
+  {
+    "revision": "3feeaa37549b4134710f66febebb3c5b",
+    "url": "/beta-code-js/static/media/tinos-latin-700.3feeaa37.woff"
+  },
+  {
+    "revision": "add6b49484bb210ee412748289e516ae",
+    "url": "/beta-code-js/static/media/tinos-latin-700italic.add6b494.woff2"
+  },
+  {
+    "revision": "6e68868302e579e7717b73a4c36227df",
+    "url": "/beta-code-js/index.html"
+  }
+];
