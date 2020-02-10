7e8d209dec30f20d223397f35ef50e8ef4f8d290
zfletch <zfletch2@gmail.com>
2020-02-10 10:22:47 -0500

    snapshot tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import renderer from 'react-test-renderer';
 
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = window.document.createElement('div');
-  render(<App />, div);
+it('renders the main page', () => {
+  const component = <App />;
+  const tree = renderer.create(component).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('converts Unicode to Beta Code', () => {
+  const component = <App />;
+  const rendered = renderer.create(component);
+  rendered.root.findByProps({ placeholder: 'μῆνιν ἄειδε θεὰ ...' }).props.onChange({
+    target: { value: 'κατέβην χθὲς εἰς Πειραιᾶ' },
+  });
+  const tree = rendered.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('converts Beta Code to Unicode', () => {
+  const component = <App />;
+  const rendered = renderer.create(component);
+  rendered.root.findByProps({ placeholder: 'mh=nin a)/eide qea ...' }).props.onChange({
+    target: { value: 'a)/ndra moi e)/nnepe' },
+  });
+  const tree = rendered.toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
